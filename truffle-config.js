b6979b7236d69731a1c2cb06d9da34318f026955
Sayan Samanta <samantasayan23@gmail.com>
2022-10-07 02:47:27 +0530

    updated network to goerli

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
 const path = require("path");
+require("dotenv").config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +11,12 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: "*",
-    }
+    },
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://goerli.infura.io/v3/'+process.env.GOERLI_API_KEY)
+      },
+      network_id: 5,
+    },
   }
 };
